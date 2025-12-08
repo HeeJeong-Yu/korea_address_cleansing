07060676162febeb3df569d4dde866812c2ce205
HeeJeong-Yu <yhj20001206@gmail.com>
2025-12-08 19:11:49 +0900

    E: 원본주소 분리, 컬럼값 통일

--- read_target_data.py
+++ read_target_data.py
@@ -9,10 +9,20 @@ def read_csv(file_path):
         print(f"csv파일 읽기 오류({file_path}): {e}")
         sys.exit(1)
 
-def split_address(data, return_type=0):
+def rename_col(df, target_col):
+    taraget_cols = get_config_data("target_col", "roadnames")
+    col_map = {col:target_col for col in taraget_cols}
+
+    try:
+        df.rename(columns=col_map,
+                inplace=True)
+    except Exception as e:
+        print("{e}, 데이터프레임 컬럼명 변경 오류")
+
+def split_address(data):
     pattern = r"(?:[^\s]+(?:개|길|리|로|동|가)\s*"+\
-               "(?:\d*[가-힣]*(?:개|길|리|로|동|가))?)"+\
-               "(?:\s+)?((지하)*|(산)*)(?:\s+)?(\d+(?:-\d*)?)" 
+            "(?:\d*[가-힣]*(?:개|길|리|로|동|가))?)"+\
+            "(?:\s+)?((지하)*|(산)*)(?:\s+)?(\d+(?:-\d*)?)" 
     match = re.search(pattern, data)
 
     if match is not None:
@@ -20,21 +30,20 @@ def split_address(data, return_type=0):
         address = data[:idx].strip()          # 도로명주소
         detailed_address = data[idx:].strip() # 상세주소
 
-        if return_type == 0: return address, detailed_address
-        elif return_type == 1: return address
-        elif return_type == 2: return detailed_address
-    else: return data if return_type == 1 else data, ''
+        return address, detailed_address
 
+    else: return data, ''
 
 def read_target_data():
     target_path = get_config_data("paths", "target_data")
     target_file_list = os.listdir(target_path)
-    taraget_col = get_config_data("target_col", "roadname")
+    target_col = get_config_data("target_col", "roadname")
 
     for filename in target_file_list:
         file_df = read_csv(os.path.join(target_path, filename))
+        # file_df = rename_col(file_df, target_col)
 
-        for col in taraget_col:
-            file_df[col]
+        # split_results = file_df['도로명주소'].apply(split_address, result_type='expand')
+        # file_df[['도로명주소', '상세주소']] = split_results
 
         break
