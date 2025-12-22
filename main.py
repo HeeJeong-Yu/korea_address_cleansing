26455e31d6b89fb06e606fc2203c866721ad5915
HeeJeong-Yu <yhj20001206@gmail.com>
2025-12-22 17:57:15 +0900

    E: col 변경 코드 수정

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from read_correct_data import *
 from utils import *
+from get_correct_data_summary import *
+from process_roadname_data import *
 
 # 주소 정보 누리집 데이터 읽어 오기
 roadname_foldername = get_config_data("foldernames", "roadname"); roadname_del_word = get_config_data("del_words", "roadname")
 detailed_foldername = get_config_data("foldernames", "detailed"); detailed_del_word = get_config_data("del_words", "detailed")
 
-log("도로명주소 한글 데이터 읽기 시작")
+log("도로명주소 한글 데이터 읽기 중")
 roadname = ReadCorrectData(); roadname.run(roadname_foldername, roadname_del_word); 
 roadname_df = roadname.df; del roadname
-log(f"도로명주소 한글 행 개수: {len(roadname_df)}")
 
-log("상세주소 한글 데이터 읽기 시작")
+log("상세주소 한글 데이터 읽기 중")
 detailed = ReadCorrectData(); detailed.run(detailed_foldername, detailed_del_word); 
 detailed_df = detailed.df; del detailed
-log(f"상세주소 표시 행 개수: {len(detailed_df)}")
+
+log("주소정보누리집 데이터 요약 중")
+get_correct_data_summary(roadname_df, detailed_df)
+
+log("도로명주소 한글 데이터 처리 중")
+roadname_df = process_roadname_data(roadname_df)
--- read_target_data.py
+++ read_target_data.py
@@ -2,24 +2,30 @@ import pandas as pd
 import sys, os, re
 from utils import *
 
-def read_csv(file_path):
+def read_csv(file_path: str)-> pd.DataFrame:
     try:
         return pd.read_csv(file_path, encoding='euc-kr')
     except Exception as e:
         print(f"csv파일 읽기 오류({file_path}): {e}")
         sys.exit(1)
 
-def rename_col(df, target_col):
+# 컬럼명이 여러 개인 경우 컬럼 중복 위험. 
+def rename_col(df: pd.DataFrame, target_col:str):
     taraget_cols = get_config_data("target_col", "roadnames")
-    col_map = {col:target_col for col in taraget_cols}
+    col_map  = {}
+    for col in df.columns:
+        if col in taraget_cols: 
+            col_map[col] = target_col
+    # col_map = {col:target_col for col in taraget_cols}
 
     try:
         df.rename(columns=col_map,
                 inplace=True)
     except Exception as e:
         print("{e}, 데이터프레임 컬럼명 변경 오류")
+        sys.exit(1)
 
-def split_address(data):
+def split_address(data: str)-> tuple:
     pattern = r"(?:[^\s]+(?:개|길|리|로|동|가)\s*"+\
             "(?:\d*[가-힣]*(?:개|길|리|로|동|가))?)"+\
             "(?:\s+)?((지하)*|(산)*)(?:\s+)?(\d+(?:-\d*)?)" 
@@ -39,11 +45,16 @@ def read_target_data():
     target_file_list = os.listdir(target_path)
     target_col = get_config_data("target_col", "roadname")
 
-    for filename in target_file_list:
+    for filename in target_file_list: 
         file_df = read_csv(os.path.join(target_path, filename))
-        # file_df = rename_col(file_df, target_col)
+        rename_col(file_df, target_col)
 
-        # split_results = file_df['도로명주소'].apply(split_address, result_type='expand')
-        # file_df[['도로명주소', '상세주소']] = split_results
+        file_df[target_col] = file_df[target_col].fillna('')
+        file_df[['도로명주소', '상세주소']] = pd.DataFrame(
+                file_df[target_col].map(split_address).tolist(),
+                index=file_df.index
+            )
+
+
 
         break
